e51426673d6a7431753a9686cb9cbc202a1d6155
Siddhant <santoshshrivastava39@gmail.com>
2022-09-23 00:35:27 +0530

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RegisterComponent } from './Components/register/register.component';
+import { SignInComponent } from './Components/sign-in/sign-in.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { ForgotComponent } from './Components/forgot/forgot.component';
+import { ResetPasswordComponent } from './Components/reset-password/reset-password.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RegisterComponent,
+    SignInComponent,
+    ForgotComponent,
+    ResetPasswordComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
